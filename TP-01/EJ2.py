30746ce7f2b30ee94651c9382bc1da8bee098836
Serosales <Serosales@uade.edu.ar>
2024-09-19 10:54:40 -0300

    validar fechas y programa de prueba

--- TP-01/EJ2.py
+++ TP-01/EJ2.py
@@ -5,12 +5,13 @@ Devolver True o False según la fecha sea correcta o no. Realizar también un
 programa para verificar el comportamiento de la función"""
 
 
-# Función para determinar si es o no un año bisiesto.
+# FUNCIÓN: es_anio_bisiesto
 def es_anio_bisiesto(anio: int) -> bool:
     """
-    Pre:
+    CONTRATO:
+    PRE::
         "anio" debe ser un entero positivo que representa el año a verificar.
-    Post:
+    POST:
         Devuelve True si el año es bisiesto.
         Devuelve False si el año no es bisiesto.
 
@@ -19,14 +20,16 @@ def es_anio_bisiesto(anio: int) -> bool:
     return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
 
 
+# FUNCIÓN:es_fecha_valida
 def es_fecha_valida(dia: int, mes: int, anio: int) -> bool:
     """
-    Pre:
+    CONTRATO:
+    PRE:
     - "dia", "mes" y "anio" deben ser enteros positivos donde "dia" y "mes" son mayores que 0,
       y "mes" debe estar en el rango de 1 a 12.
     - "anio" debe ser un entero positivo.
 
-    Post:
+    POST:
         Devuelve True si la combinación de "dia", "mes", y "anio" corresponde a una fecha válida.
         Devuelve False si la combinación no representa una fecha válida.
     """
@@ -46,19 +49,21 @@ def es_fecha_valida(dia: int, mes: int, anio: int) -> bool:
         return False
 
     # Verificar si el día es válido para el mes.
-    
+
     if dia < 1 or dia > dias_por_mes[mes - 1]:
         return False
 
     return True
 
 
+# FUNCIÓN: ingrese_los_numeros
 def ingrese_los_numeros() -> tuple:
     """
-     Pre:
+    CONTRATO:
+     PRE:
         Se asume que el usuario ingresará valores enteros para día, mes y año.
 
-    Post:
+    POST:
         Devuelve una tupla (dia, mes, anio) donde "dia", "mes" y "anio" son enteros positivos
         ingresados por el usuario.
 
@@ -70,22 +75,24 @@ def ingrese_los_numeros() -> tuple:
 
     return (dia, mes, anio)
 
-    # Programa principal
-def main() -> str:
+
+# FUNCIÓN: main
+def main() -> None:
     """
-    Pre:
-        Función `main`.
-    Post:
+    CONTRATO:
+    PRE:
+        Función "main".
+    POST:
         Imprime un mensaje indicando si la fecha  es válida o no.
+        No tiene parametros de retorno.
     """
     dia, mes, anio = ingrese_los_numeros()
 
     if es_fecha_valida(dia, mes, anio):
-        return "La fecha es válida."
+        print("La fecha es válida.")
     else:
-        return "La fecha no es válida."
+        print("La fecha no es válida.")
 
 
 if __name__ == "__main__":
-    resultado = main()
-    print(resultado)
+    main()
