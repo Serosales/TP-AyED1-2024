6006ee5977c82dc2a7800678a1545b2c290b9083
Serosales <Serosales@uade.edu.ar>
2024-09-21 21:08:50 -0300

    Mejora de implementación y contratos

--- TP-02/EJ9.py
+++ TP-02/EJ9.py
@@ -1,13 +1,15 @@
 """ Generar e imprimir una lista por comprensión entre A y B con los múltiplos de 7
 que no sean múltiplos de 5. A y B se ingresar desde el teclado. """
 
+
 # FUNCIÓN: ingreso_num
-def ingreso_num() -> tuple:
+def ingreso_num() -> tuple[int, int]:
     """
     CONTRATO:
-    PRE: 
+    PRE:
     No hay condiciones específicas de entrada.
-    POST: 
+    EL usuario debe ingresar un valor "a" y un valor "b" ambos valores deben de ser enteros.
+    POST:
     Retorna una tupla (a, b) con los valores ingresados por el usuario.
     """
 
@@ -15,21 +17,27 @@ def ingreso_num() -> tuple:
     b = int(input("Introduce el valor de B: "))
     return (a, b)
 
+
 # FUNCIÓN: main
 def main() -> None:
     """
     CONTRATO:
      PRE:
         Los valores A y B se ingresan desde el teclado.
-     POST: 
+     POST:
         Imprime la lista de múltiplos de 7 que no sean múltiplos de 5 en el rango [A, B].
+        No tiene  parametros de retorno.
     """
     # Leer los valores de A y B
     a, b = ingreso_num()
-    
+    if a > b:
+        print(f"EL número A : {a} tiene qeue ser menor o igual que B:  {b}\n ")
+        print("ingrese valores validos")
+
     # Generar la lista de múltiplos de 7 que no sean múltiplos de 5 en el rango [A, B]
     multiples = [num for num in range(a, b + 1) if num % 7 == 0 and num % 5 != 0]
     print(multiples)
+
 
 if __name__ == "__main__":
     main()
